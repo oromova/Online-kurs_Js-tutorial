b086be6006e9060d2d3b8ced22a08671dabef1dc
Yulduz_Oromova <mahmurbekovna@gmail.com>
2024-05-09 01:08:31 +0300

    Object - 3. Masala yechish

---
+++ 8-dars. Object - 3/script.js
@@ -0,0 +1,29 @@
+// ========== Object. Masala ishlash. =========
+
+// let obj = {
+//   name: 'webbrain',
+//   surname: 'academy',
+//   age: 123,
+//   status: false,
+// }
+
+// let res = {}
+
+// for (k in obj ) {
+//   if(typeof obj[k] === 'string'){
+//     res[k] = obj[k]
+//   }
+// }
+// console.log(res);     // { name: 'webbrain', surname: 'academy' }
+
+// 2-yo'l
+
+// for ( k in obj ) {
+//   // console.log(obj[k]);   // webbrain, academy ,123, false
+// if (typeof obj[k] === 'string') {
+//   res = {...res, [k]: obj[k]};
+ 
+// }
+// }
+// console.log(res);   // res { name: 'webbrain', surname: 'academy' }
+
